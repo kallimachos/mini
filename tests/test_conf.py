7bf9390a3416bb22e1815c414986d7b0378f8d61
kallimachos <kallimachos@gmail.com>
2015-12-21 16:21:25 +1000

    try travis again

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -8,6 +8,6 @@ configfile = 'tests/config4tests.ini'
 def test_loadConfig():
     settings = conf.LoadConfig(configfile)
     assert settings.protocol == 'http'
-    assert settings.host == '0.0.0.0'
+    assert settings.host == '192.168.0.17'
     assert settings.port == '8080'
-    assert settings.indexURL == 'http://0.0.0.0:8080/'
+    assert settings.indexURL == 'http://192.168.0.17:8080/'
