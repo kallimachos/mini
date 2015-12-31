6a10ebf43a475a823d7c4dd0563d46e8b0f86f88
kallimachos <kallimachos@gmail.com>
2015-12-31 12:12:21 +1000

    Add sqlite_version to model.py

--- mini/model.py
+++ mini/model.py
@@ -2,28 +2,40 @@
 """Model for mini."""
 
 import json
+import sqlite3 as sql
 
 
-def create():
-    """Create a DB."""
-    pass
-
-
-def add():
+def add(database, data):
     """Add an item to the DB."""
     pass
 
 
-def dump():
+def delete(database, data):
+    """Delete an item from the DB."""
+    pass
+
+
+def dump(database):
     """
     Return a raw dump of the DB.
 
     :returns: a raw dump of the DB
     :rtype: JSON
     """
-    resp = json.dumps('This is a string that represents a dumped DB')
+    resp = json.dumps('DB dump test')
     return(resp)
 
 
+def sqlite_version(database):
+    """Return the SQLite version of a database."""
+    con = sql.connect(database)
+    with con:
+        cur = con.cursor()
+        cur.execute('SELECT SQLITE_VERSION()')
+        data = cur.fetchone()
+        resp = json.dumps('SQLite version: %s' % data)
+        return(resp)
+
+
 if __name__ == '__main__':
-    print(dump())
+    print(json.loads(sqlite_version('test.db')))
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,23 @@
 #!/bin/python3
 
-# import model
-#
-# configfile = 'tests/config4tests.ini'
-#
-#
-# def test_dump():
-#     assert model.dump() == 'This is a string that represents a dumped DB'
+import model
+
+configfile = 'tests/config4tests.ini'
+database = 'mini/test.db'
+data = 'Hello'
+
+
+def test_add():
+    assert model.add(database, data) is None
+
+
+def test_delete():
+    assert model.delete(database, data) is None
+
+
+def test_dump():
+    assert model.dump(database) == '"DB dump test"'
+
+
+def test_sqlite_version():
+    assert model.sqlite_version(database) == '"SQLite version: 3.9.0"'
